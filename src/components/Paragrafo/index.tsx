c9c021a49e9b53568ed3f0b3e90909a4d74597cd
Edmilson Santos <edsouza.js@gmail.com>
2023-07-02 17:34:10 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/components/Paragrafo/index.tsx
@@ -0,0 +1,13 @@
+import { isTypeAssertionExpression } from 'typescript'
+import { P } from './styles'
+
+export type Props = {
+  children: string
+  tipo?: 'principal' | 'secundario'
+}
+
+const Paragrafo = ({ children, tipo = 'principal' }: Props) => (
+  <P tipo={tipo}>{children}</P>
+)
+
+export default Paragrafo
---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="http://github.com/dhymas1st.png" />
+    <Titulo fontSize={20}>Dhymas Diamond</Titulo>
+  </aside>
+)
+
+export default Sidebar
