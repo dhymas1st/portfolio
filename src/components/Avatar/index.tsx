5e19446250e9777c94f747b46cd11a1f0d6f2c3c
Edmilson Santos <edsouza.js@gmail.com>
2023-07-02 20:49:51 -0300

    estilizando a Sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/dhymas1st.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="http://github.com/dhymas1st.png" />
-    <Titulo fontSize={20}>Dhymas Diamond</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Dhymas Diamond</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        dhymas1st
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
